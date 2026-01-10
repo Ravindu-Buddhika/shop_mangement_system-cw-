f66973f20a74b837dd22c93a4373a0ecb01e1392
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-10 16:50:20 +0530

    created form and add handle eddit fuction

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-// function loadDAta() {
-//     try {
-//         fetch(`https://dummyjson.com/products?limit=10`)
-//             .then(res => res.json)
-//             .then(data => console.log(data.products))
-
-//     } catch (error) {
-//         console.error("Failed to fetch data:", error);
-//     }
-// }
-
-// window.onload = loadDAta;
-
 async function loadDAta() {
+    const loader = document.getElementById('loading-state');
+    const tableContainer = document.getElementById('table-container');
+
     try {
+        loader.style.display = 'block';
+        tableContainer.style.display = 'none';
+
         const response = await fetch('https://dummyjson.com/products?limit=10');
         const data = await response.json();
+        
         console.log("Products loaded:", data.products);
 
-        // NEXT STEP: Call a function here to put this data into your table
-        // displayProducts(data.products);
+        displayTable(data.products);
+
+        loader.style.display = 'none';
+        tableContainer.style.display = 'block';
 
     } catch (error) {
         console.error("Failed to fetch data:", error);
@@ -27,3 +23,31 @@ async function loadDAta() {
 }
 
 window.onload = loadDAta;
+
+const displayTable = (products) => {
+    const tableBody = document.getElementById('product-list');
+    tableBody.innerHTML = "";
+
+    products.forEach(product => {
+        const row = `
+            <tr>
+                <td><img src="${product.thumbnail}" alt="${product.title}" width="50" class="rounded"></td>
+                <td>${product.title}</td>
+                <td>$${product.price}</td>
+                <td><span class="badge bg-secondary">${product.category}</span></td>
+                <td>
+                    <button class="btn btn-sm btn-primary" onclick="handleEdit(${product.id}, '${product.title}', ${product.price}, '${product.category}')">Edit</button>
+                    <button class="btn btn-sm btn-danger" onclick="deleteProduct(${product.id}, this)">Delete</button>
+                </td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+};
+
+function handleEdit(id, title, price, category){
+    document.getElementById('prod-id').value = id;
+    document.getElementById('prod-title').value = title;
+    document.getElementById('prod-price').value = price;
+    document.getElementById('prod-category').value = category;
+}
